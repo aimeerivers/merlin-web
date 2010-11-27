8bb34cb049f53aaad5334c1fae40ffee4b51c67d
aimee daniells <aimee@edendevelopment.co.uk>
2010-11-27 11:46:49 +0000

    New Rails 3.0.3 application for Merlin's Castle framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merlin::Application.config.secret_token = '38b13fb533ee221bb36676efc8ae2c476c38a1aa5fb49d2993b3bca35a1d6899d4e8bf1f56be791401d3c6de8d93a730b403d0f3b35180d2558fd8b6ae5ecd81'
