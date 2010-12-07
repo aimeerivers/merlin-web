50c7f24c01dd7e7fd190ebf6179bb4e2620a4338
Chris Parsons and aimee daniells <dev+chrismdp+sermoa@edendevelopment.co.uk>
2010-12-07 20:24:17 +0000

    Remove unnecessary relational database files.

--- db/migrate/20101201211830_add_restriction_to_pathway.rb
+++
@@ -1,9 +0,0 @@
-class AddRestrictionToPathway < ActiveRecord::Migration
-  def self.up
-    add_column :pathways, :restriction, :string
-  end
-
-  def self.down
-    remove_column :pathways, :restriction
-  end
-end
