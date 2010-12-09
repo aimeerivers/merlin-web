450162aa476da65cf3371f79f85fdd65dd2d9093
aimee daniells <aimee@edendevelopment.co.uk>
2010-12-09 20:06:50 +0000

    Mongo initialization for Heroku/MongoHQ.

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,8 +1,7 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "#merlin-#{Rails.env}"
-
-if defined?(PhusionPassenger)
-   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-     MongoMapper.connection.connect_to_master if forked
-   end
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(RAILS_ENV)
+else
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.database = "#merlin-#{Rails.env}"
 end
