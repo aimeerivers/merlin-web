4efb441cc962292cb1f5fac9944b6277031eba53
aimee daniells <aimee@edendevelopment.co.uk>
2010-12-19 21:02:00 +0000

    Oops, do not want borders on the direction arrows.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def direction_arrow(direction, available_directions)
     content_tag('div', class: "go-#{direction}") do
       if available_directions.include? direction
-        link_to(image_tag("go-#{direction}.jpg", :alt => "Go #{direction.titlecase}", :title => "Go #{direction.titlecase}"), move_path(direction)) 
+        link_to(image_tag("go-#{direction}.jpg", :alt => "Go #{direction.titlecase}", :title => "Go #{direction.titlecase}"), move_path(direction), :class => 'direction-arrow')
       end
     end
   end
